0411aff16c6b64fd3f837166839ed1c255d3ad94
bmm853 <rebeccawnelson@gmail.com>
2023-07-24 18:16:45 -0500

    Completed studio requirements

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // TODO: add code here
+window.addEventListener("load", function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        response.json().then(function(data) {
+            const container = document.getElementById("container");
+            for (let i = 0; i < data.length; i++) {
+                container.innerHTML += `
+                <div class="astronaut">
+                    <div class="bio">
+                        <h3>${data[i].firstName} ${data[i].lastName}</h3>
+                        <ul>
+                            <li>Hours in space: ${data[i].hoursInSpace}</li>
+                            <li>Active: ${data[i].active}</li>
+                            <li>Skills: ${data[i].skills.join(", ")}</li>
+                        </ul>
+                    </div>
+                    <img class="avatar" src="images/${data[i].picture}">
+                </div>
+            `;
+            }
+        });
+     });
+});
